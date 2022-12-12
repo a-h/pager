86e46342f853b17d17add6be733050cb5d6219b5
Adrian Hesketh <adrianhesketh@hushmail.com>
2022-12-12 11:05:26 +0000

    refactor: use Channel and Func explicitly

--- pager.go
+++ pager.go
@@ -1,25 +1,49 @@
 package pager
 
-// New creates a pager to iterate through pages.
-func New[T any](data []T, pageSize int) chan []T {
+// Channel creates a channel to iterate through pages.
+// Use Func for 20x better performance and fewer allocations at the cost
+// of slightly worse syntax.
+func Channel[T any](data []T, pageSize int) chan []T {
 	if pageSize < 1 {
 		panic("page size cannot be less than zero")
 	}
 	rv := make(chan []T)
-	var index int
 	go func() {
-		for {
-			if index >= len(data) {
-				close(rv)
-				return
-			}
-			end := index + pageSize
+		defer close(rv)
+		pages, remainder := len(data)/pageSize, len(data)%pageSize
+		if remainder > 0 {
+			pages++
+		}
+		for i := 0; i < pages; i++ {
+			end := i*pageSize + pageSize
 			if end > len(data) {
 				end = len(data)
 			}
-			rv <- data[index:end]
-			index += pageSize
+			rv <- data[i*pageSize : end]
 		}
 	}()
 	return rv
+}
+
+// Func uses a function parameter to iterate through pages.
+// It's 20x faster than the channel variant, using fewer allocations.
+func Func[T any](data []T, pageSize int, f func([]T) error) (err error) {
+	if pageSize < 1 {
+		panic("page size cannot be less than zero")
+	}
+	pages, remainder := len(data)/pageSize, len(data)%pageSize
+	if remainder > 0 {
+		pages++
+	}
+	for i := 0; i < pages; i++ {
+		end := i*pageSize + pageSize
+		if end > len(data) {
+			end = len(data)
+		}
+		err = f(data[i*pageSize : end])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- pager_test.go
+++ pager_test.go
@@ -1,63 +1,109 @@
-package pager
+package pager_test
 
 import (
+	"io"
 	"testing"
 
+	"github.com/a-h/pager"
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestPager(t *testing.T) {
-	tests := []struct {
-		name     string
-		pager    chan []string
-		expected [][]string
-	}{
-		{
-			name:     "no data",
-			pager:    New([]string{}, 3),
-			expected: nil,
+var tests = []struct {
+	name     string
+	data     []string
+	pageSize int
+	expected [][]string
+}{
+	{
+		name:     "no data",
+		data:     []string{},
+		pageSize: 3,
+		expected: nil,
+	},
+	{
+		name:     "quantity of data less than page size",
+		data:     []string{"a", "b"},
+		pageSize: 3,
+		expected: [][]string{
+			{"a", "b"},
 		},
-		{
-			name:  "small quantity of data than page size",
-			pager: New([]string{"a", "b"}, 3),
-			expected: [][]string{
-				[]string{"a", "b"},
-			},
+	},
+	{
+		name:     "input same length as page size",
+		data:     []string{"a", "b", "c"},
+		pageSize: 3,
+		expected: [][]string{
+			{"a", "b", "c"},
 		},
-		{
-			name:  "input same length as page size",
-			pager: New([]string{"a", "b", "c"}, 3),
-			expected: [][]string{
-				[]string{"a", "b", "c"},
-			},
+	},
+	{
+		name:     "two pages",
+		data:     []string{"a", "b", "c"},
+		pageSize: 2,
+		expected: [][]string{
+			{"a", "b"},
+			{"c"},
 		},
-		{
-			name:  "two pages",
-			pager: New([]string{"a", "b", "c"}, 2),
-			expected: [][]string{
-				[]string{"a", "b"},
-				[]string{"c"},
-			},
+	},
+	{
+		name:     "three pages",
+		data:     []string{"a", "b", "c"},
+		pageSize: 1,
+		expected: [][]string{
+			{"a"},
+			{"b"},
+			{"c"},
 		},
-		{
-			name:  "three pages",
-			pager: New([]string{"a", "b", "c"}, 1),
-			expected: [][]string{
-				[]string{"a"},
-				[]string{"b"},
-				[]string{"c"},
-			},
-		},
-	}
+	},
+}
+
+func TestChannel(t *testing.T) {
 	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
+		t.Run("Channel: "+test.name, func(t *testing.T) {
 			var actual [][]string
-			for page := range test.pager {
+			for page := range pager.Channel(test.data, test.pageSize) {
 				actual = append(actual, page)
 			}
 			if diff := cmp.Diff(test.expected, actual); diff != "" {
 				t.Error(diff)
 			}
+		})
+	}
+}
+
+func TestFunc(t *testing.T) {
+	for _, test := range tests {
+		t.Run("Func: "+test.name, func(t *testing.T) {
+			var actual [][]string
+			err := pager.Func(test.data, test.pageSize, func(page []string) error {
+				actual = append(actual, page)
+				return nil
+			})
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if diff := cmp.Diff(test.expected, actual); diff != "" {
+				t.Error(diff)
+			}
+		})
+	}
+}
+
+func BenchmarkChannel(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		for page := range pager.Channel(tests[3].data, tests[3].pageSize) {
+			io.Discard.Write([]byte(page[0]))
+		}
+	}
+}
+
+func BenchmarkFunc(b *testing.B) {
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		pager.Func(tests[3].data, tests[3].pageSize, func(page []string) error {
+			io.Discard.Write([]byte(page[0]))
+			return nil
 		})
 	}
 }
